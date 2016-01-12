8cc51b2a6456e15fe53f4d2b7fb9c731229d8906
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-01-12 18:12:27 +0000

    Nineth Test Passed & Refactored

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,12 +1,9 @@
 def fizzbuzz(number)
-	if (number % 15) == 0
-		'fizzbuzz'
-	elsif (number % 3) == 0
-		'fizz'
-	elsif (number % 5) == 0
-		'buzz'
-	else
-		number
-	end
-
+	return 'fizzbuzz' if (number % 15) == 0
+		
+	return 'fizz' if (number % 3) == 0
+		
+	return 'buzz' if (number % 5) == 0
+		
+	number
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -13,7 +13,7 @@ describe 'fizzbuzz' do
  		expect(fizzbuzz(5)).to eq 'buzz'
  	end
 
- 	(1..50).each do |i|
+ 	(1..100).each do |i|
  		if i % 3 == 0 && i % 5 != 0 
  			it 'returns "fizz" when passed any multiple of 3 except when also a multiple of 5' do
  				expect(fizzbuzz(i)).to eq 'fizz'
@@ -22,14 +22,14 @@ describe 'fizzbuzz' do
  	end
 
 it 'returns "fizzbuzz" when passed a multiple of both 3 and 5' do	
-	(1..50).each do |i| #loop on the inside will only return one test example (1 success or 1 failure)
+	(1..100).each do |i| #loop on the inside will only return one test example (1 success or 1 failure)
 		if i % 3 == 0 && i % 5 == 0
 			expect(fizzbuzz(i)).to eq 'fizzbuzz'
 		end
 	end
 end
 
-(1..50).each do |i| #keeping the each loop on the outside produces more test examples
+(1..100).each do |i| #keeping the each loop on the outside produces more test examples
 	it 'returns "buzz" when passed a multiple of 5 except those divisible by 3' do
 		if i % 5 == 0 && i % 3 != 0
 			expect(fizzbuzz(i)).to eq 'buzz'
@@ -37,7 +37,7 @@ end
 	end
 end
 
-(1..50).each do |i|
+(1..100).each do |i|
 	it 'returns the number itself if not divisible by 3 or 5' do
 		if i % 5 != 0 && i % 3 != 0
 			expect(fizzbuzz(i)).to eq i
