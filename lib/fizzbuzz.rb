14f81dc1e4828c7e7ee34851c48c9e5625a9e6a0
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-01-12 17:13:13 +0000

    Third test passed

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -3,5 +3,9 @@ def fizzbuzz(number)
 		1
 	elsif number == 3
 		'fizz'
+
+	elsif number == 5
+		'buzz'
 	end
+
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,7 +8,10 @@ describe 'fizzbuzz' do
 	it 'returns 1 when passed 1 ' do
 		expect(fizzbuzz(1)).to eq 1
 	end
-
+ 	
+ 	it 'returns "buzz" when passed 5' do
+ 		expect(fizzbuzz(5)).to eq 'buzz'
+ 	end
 
 
 end
